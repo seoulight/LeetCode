c5a207a90192115ef6771cdd7743284a7383dcfb
gyeonghyun kim <60089158+gynhyunkim@users.noreply.github.com>
2023-05-10 21:32:09 +0900

    Time: 3 ms (93.39%), Space: 43.2 MB (38.71%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,7 +1,9 @@
 class Solution {
     
     public boolean isVowels(char c) {
-        return "aeiouAEIOU".contains(String.valueOf(c));
+        // return "aeiouAEIOU".contains(String.valueOf(c));
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
+            || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
     }
     
     public void swap(char[] chars, int x, int y) {
