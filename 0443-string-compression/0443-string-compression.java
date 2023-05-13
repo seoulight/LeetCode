6f41d0420714988b93b4731587fca22e608b3236
gyeonghyun kim <60089158+gynhyunkim@users.noreply.github.com>
2023-05-13 12:18:39 +0900

    Time: 1 ms (96.80%), Space: 42.2 MB (64.43%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int compress(char[] chars) {
+        int i = 0, totalLen = 0, nextIdx = 0;
+        StringBuilder sb = new StringBuilder();
+        
+        while (i < chars.length) {
+            int cnt = 1;
+            char c = chars[i++];
+            
+            while (i < chars.length && chars[i] == c) {
+                i++;
+                cnt++;
+            }
+            chars[nextIdx++] = c;
+            totalLen++;
+            if (cnt > 1) {
+                for (char num : String.valueOf(cnt).toCharArray()) {
+                    chars[nextIdx++] = num;
+                    totalLen++;
+                }
+            }
+        }
+        return totalLen;
+    }
+}
