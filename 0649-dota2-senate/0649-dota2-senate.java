3e4ab76a9af8ea68b3aca44e5a01b6c49a3824f6
gyeonghyun kim <60089158+gynhyunkim@users.noreply.github.com>
2023-05-30 15:50:17 +0900

    Time: 13 ms (40.99%), Space: 44.4 MB (7.17%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String predictPartyVictory(String senate) {
+        Queue<Integer> q1 = new LinkedList<>(), q2 = new LinkedList<>();
+        int n = senate.length();
+        
+        for (int i = 0; i < n; i++) {
+            if (senate.charAt(i) == 'R') {
+                q1.add(i);
+            } else {
+                q2.add(i);
+            }
+        }
+        while (!q1.isEmpty() && !q2.isEmpty()) {
+            int r_index = q1.poll(), d_index = q2.poll();
+            if (r_index < d_index) {
+                q1.add(r_index + n);
+            } else {
+                q2.add(d_index + n);
+            }
+        }
+        return (q1.size() > q2.size()) ? "Radiant" : "Dire";
+    }
+}
