03d78525e28f9a1c82ffe6051819f81fbf5244a4
Gyeonghyun Kim <60089158+gynhyunkim@users.noreply.github.com>
2024-01-10 10:19:53 +0900

    Time: 374 ms (93.30%), Space: 63.2 MB (76.35%) - LeetHub

---
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.py
@@ -0,0 +1,35 @@
+from collections import defaultdict, deque
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def amountOfTime(self, root: Optional[TreeNode], start: int) -> int:
+        def dfs(node):
+            if node is None:
+                return
+            if node.left:
+                graph[node.val].append(node.left.val)
+                graph[node.left.val].append(node.val)
+            if node.right:
+                graph[node.val].append(node.right.val)
+                graph[node.right.val].append(node.val)
+            dfs(node.left)
+            dfs(node.right)
+        
+        graph = defaultdict(list)
+        dfs(root)
+        visited = set()
+        q = deque([start])
+        time = -1
+        while q:
+            time += 1
+            for _ in range(len(q)):
+                current_node = q.popleft()
+                visited.add(current_node)
+                for neighbor in graph[current_node]:
+                    if neighbor not in visited:
+                        q.append(neighbor)
+        return time
