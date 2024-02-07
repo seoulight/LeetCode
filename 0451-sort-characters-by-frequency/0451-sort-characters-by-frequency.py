85d37c2df82a0066e5c8507f35ae03592a2106da
Gyeonghyun Kim <60089158+gynhyunkim@users.noreply.github.com>
2024-02-07 13:50:04 +0900

    Time: 49 ms (53.32%), Space: 17.9 MB (66.85%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -1,12 +1,18 @@
 from collections import Counter
+from heapq import heapify, heappop
 
 class Solution:
     def frequencySort(self, s: str) -> str:
         word_count = Counter(s)
-        sorted_count = sorted(word_count.items(), key = lambda x:x[1], reverse = True)
+        # sorted_count = sorted(word_count.items(), key = lambda x:x[1], reverse = True)
+        max_heap = [(-freq, char) for char, freq in word_count.items()]
+        heapify(max_heap)
         answer = ""
-        for key, value in sorted_count:
-            answer += key * value
+        while max_heap:
+            freq, char = heappop(max_heap)
+            answer += char * -freq
+        # for key, value in sorted_count:
+        #     answer += key * value
         return answer
             
         
