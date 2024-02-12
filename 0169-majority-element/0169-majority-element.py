9b7f0dff1e9392c3205f30463e8dfd9e6d7b4805
Gyeonghyun Kim <60089158+gynhyunkim@users.noreply.github.com>
2024-02-12 23:57:10 +0900

    Time: 128 ms (88.95%), Space: 18 MB (63.51%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,6 @@
+from collections import Counter
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = Counter(nums)
+        sorted_count = sorted(count.items(), key=lambda x:x[1], reverse=True)
+        return sorted_count[0][0]
