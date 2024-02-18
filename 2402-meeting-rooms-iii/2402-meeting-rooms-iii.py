000bbd1d824c54286fa2f1ec285a7e5578f102e4
Gyeonghyun Kim <60089158+gynhyunkim@users.noreply.github.com>
2024-02-18 23:27:44 +0900

    Time: 1279 ms (36.79%), Space: 62.3 MB (90.33%) - LeetHub

---
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.py
@@ -0,0 +1,15 @@
+from heapq import heappop, heappush
+class Solution:
+    def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
+        rooms = [[0, i] for i in range(n)]
+        ans = [0] * n
+        meetings.sort(key = lambda x : [x[0], x[1]])
+        for curstart, curend in meetings:
+            while rooms and rooms[0][0] < curstart:
+                _, prevroom = heappop(rooms)
+                heappush(rooms, [curstart, prevroom])
+            prevend, prevroom = heappop(rooms)
+            heappush(rooms, [prevend + (curend - curstart), prevroom])
+            ans[prevroom] += 1
+        return ans.index(max(ans))
+                
