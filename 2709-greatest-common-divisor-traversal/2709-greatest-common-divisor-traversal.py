02054fa93dcc410f2beb3479ad065e70025db13d
Gyeonghyun Kim <60089158+gynhyunkim@users.noreply.github.com>
2024-02-25 22:10:10 +0900

    Time: 7564 ms (31.25%), Space: 48.9 MB (6.25%) - LeetHub

---
+++ 2709-greatest-common-divisor-traversal/2709-greatest-common-divisor-traversal.py
@@ -0,0 +1,41 @@
+from collections import defaultdict, deque
+
+class Solution:
+    def gcd(self, num1: int, num2: int) -> int:
+        large = max(num1, num2)
+        small = min(num1, num2)
+        
+        while large % small > 0:
+            small = large % small
+        return small
+        
+    def canTraverseAllPairs(self, nums: List[int]) -> bool:
+        prime2index = defaultdict(list)
+        index2prime = defaultdict(list)
+        for i, n in enumerate(nums):
+            temp = n
+            for j in range(2, int(n ** 0.5) + 1):
+                if temp % j == 0:
+                    prime2index[j].append(i)
+                    index2prime[i].append(j)
+                while temp % j == 0:
+                    temp //= j
+            if temp > 1:
+                prime2index[temp].append(i)
+                index2prime[i].append(temp)
+                
+        q = deque([0])
+        visited_index = [False] * len(nums)
+        visited_prime = []
+        while q:
+            curr = q.popleft()
+            visited_index[curr] = True
+            for prime in index2prime[curr]:
+                if prime not in visited_prime:
+                    visited_prime.append(prime)
+                    for index in prime2index[prime]:
+                        if not visited_index[index]:
+                            q.append(index)
+            
+        return False not in visited_index
+        
