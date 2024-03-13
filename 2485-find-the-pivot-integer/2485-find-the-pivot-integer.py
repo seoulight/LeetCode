13e87208538b6f688e75fb5bfae2c6095d4ef8b6
Gyeonghyun Kim <60089158+seoulight@users.noreply.github.com>
2024-03-13 17:32:46 +0900

    Time: 30 ms (97.37%), Space: 16.5 MB (59.17%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
@@ -1,9 +1,7 @@
 class Solution:
     def pivotInteger(self, n: int) -> int:
-        prefix_sum = [0] * (n + 1)
-        for i in range(1, n + 1):
-            prefix_sum[i] = prefix_sum[i - 1] + i
-        for pivot in range(n, 0, -1):
-            if prefix_sum[n] - prefix_sum[pivot - 1] == prefix_sum[pivot]:
-                return pivot
+        _sum = n * (n + 1) // 2
+        x = math.sqrt(_sum)
+        if x - math.ceil(x) == 0:
+            return int(x)
         return -1
